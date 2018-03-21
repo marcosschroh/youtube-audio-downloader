fef13a7c915bc93fec7ee0e94f5700c4b1c42112
Marcos Schroh <schrohm@gmail.com>
2018-03-21 18:53:22 +0100

    perf(Custom Command): Custom Command has been replaced with click package.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="youtube-audio-downloader",
-    version="1.1.4",
+    version="1.1.5",
     description="Download audio from YouTube",
     keywords=["youtube-downloader", "downloader", "YouTube", "youtube", "download", "audio"],
     author="marcosschroh",
@@ -16,7 +16,8 @@ setup(
     download_url="https://github.com/marcosschroh/youtube-audio-downloader.git",
     install_requires=[
         "youtube-dl==2017.11.15",
-        "pafy==0.5.0"
+        "pafy==0.5.0",
+        "click==6.7"
     ],
     scripts=["scripts/sound-down"],
     packages=["sound_downloader"],
